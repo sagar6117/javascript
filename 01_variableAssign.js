81b7fda7dcb055d9646ce52d4629660eb49e9e9b
sagar6117 <sagarhs6117@gmail.com>
2023-12-26 15:51:05 +0530

    Assignment 26th Dec 2023
    Assignment 26th Dec 2023

---
+++ 01_variableAssign.js
@@ -0,0 +1,23 @@
+var firstName="Sagar"
+var midName="Ramesh"
+var lastName="Chaudhari"
+var age="32"
+var address="Pune City"
+var pincode="411061"
+var gender="Male"
+var isMarried= "Yes"
+
+console.log("Assignment_3")
+myName="Sagar Ramesh Chaudhari"
+console.log("Full Name:", firstName, midName, lastName );
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Pincode:", pincode);
+console.log("Gender:", gender);
+console.log("Is Married:", isMarried);
+
+
+
+
+
+
